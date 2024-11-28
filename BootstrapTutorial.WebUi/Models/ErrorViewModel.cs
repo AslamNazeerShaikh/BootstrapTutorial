b7c684516d7dc4963eedcefe88ff2c3638f30c8c
AslamNazeerShaikh <Aslam.Nazeer.Shaikh@outlook.in>
2024-11-28 16:45:03 +0530

    Added MVC Project Layer.

---
+++ BootstrapTutorial.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BootstrapTutorial.WebUi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
